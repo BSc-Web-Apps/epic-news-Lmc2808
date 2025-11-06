a779108a9d7180ab090b82a232ebaf4e61ca5300
Lmc2808 <152405599+Lmc2808@users.noreply.github.com>
2025-11-06 12:10:21 +0000

    Lesson 1 Team Member Card

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import Document from './components/shared-layout/Document.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
+import portrait1 from '~/assets/jpg/portrait-01.jpg'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
@@ -21,7 +22,21 @@ export default function App() {
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+						<h1 className="text-mega">This is Epic News</h1>
+						<p className="text-base text-white md:text-lg lg:text-xl">
+							Welcome to Epic News, where the latest developments in tech are
+							found here!
+						</p>
+						<img
+							className="h-60 w-60 rounded-full object-cover"
+							src={portrait1}
+							alt="Portrait1"
+						/>
+						<h2 className="mb-1 text-white">Bob Green</h2>
+						<h3 className="text-gray-500">Senior Designer</h3>
+						<button className="lg:text:xl rounded bg-red-500 px-4 py-2 font-bold text-white shadow-xl hover:cursor-pointer hover:bg-amber-500 md:text-lg">
+							Breaking News
+						</button>
 					</main>
 				</div>
 			</div>
