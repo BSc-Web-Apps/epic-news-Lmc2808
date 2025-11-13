005b3cdc1d1e07da55cfa5e915f64b93207ee721
Lmc2808 <152405599+Lmc2808@users.noreply.github.com>
2025-11-13 10:18:59 +0000

    3 Placeholder Images

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,34 @@ export const links: Route.LinksFunction = () => {
 export { meta } from './__root.client.tsx'
 export { headers, loader } from './__root.server.tsx'
 
+interface TeamMemberCardProps {
+	name: string
+	role: string
+	imgSrc: string
+}
+
+export function TeamMemberCard({ name, role, imgSrc }: TeamMemberCardProps) {
+	return (
+		<div className="w-fit rounded-lg bg-slate-800 p-8">
+			<img
+				src={imgSrc}
+				alt="An employee"
+				className="mx-auto h-64 w-64 rounded-full"
+			/>
+
+			<div className="pt-6">
+				<h3 className="font-semi-bold text-center text-white">{name}</h3>
+				<p className="pt-1 text-center text-slate-400">{role}</p>
+
+				<div className="flex justify-center gap-4 pt-6 text-slate-400">
+					<RiTwitterXFill />
+					<RiLinkedinBoxFill />
+				</div>
+			</div>
+		</div>
+	)
+}
+
 export default function App() {
 	const data = useLoaderData<typeof loader | null>()
 	const nonce = useNonce()
@@ -27,27 +55,21 @@ export default function App() {
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">This is Epic News</h1>
 
-						<div className="w-fit rounded-lg bg-slate-800 p-8">
-							<img
-								src={portrait1}
-								alt="An employee"
-								className="mx-auto h-64 w-64 rounded-full"
-							/>
-
-							<div className="pt-6">
-								<h3 className="font-semi-bold text-center text-white">
-									Leonard Krasner
-								</h3>
-								<p className="pt-1 text-center text-slate-400">
-									Senior Designer
-								</p>
-
-								<div className="flex justify-center gap-4 pt-6 text-slate-400">
-									<RiTwitterXFill />
-									<RiLinkedinBoxFill />
-								</div>
-							</div>
-						</div>
+						<TeamMemberCard
+							name="Ben Green"
+							role="Trainee Assistant to the Assistant Manager"
+							imgSrc={portrait2}
+						/>
+						<TeamMemberCard
+							name="Theodore Ruth"
+							role="Junior Assistant"
+							imgSrc={portrait1}
+						/>
+						<TeamMemberCard
+							name="Jimmy Smith"
+							role="Assistant to the Assistant Manager"
+							imgSrc={portrait3}
+						/>
 					</main>
 				</div>
 			</div>
