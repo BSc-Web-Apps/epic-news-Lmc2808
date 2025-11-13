9f39ebdcb301ac461ea1c746de043ed2d8d90bd9
Lmc2808 <152405599+Lmc2808@users.noreply.github.com>
2025-11-13 11:58:22 +0000

    Footer/Header

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
+import FooterLogoCentre from './components/organisms/Footer/FooterLogoCentre.tsx'
+import HeaderWithSearch from './components/organisms/HeaderWithSearch'
 import Document from './components/shared-layout/Document.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
@@ -53,9 +55,10 @@ export default function App() {
 	return (
 		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
 			<div className="flex h-screen flex-col justify-between">
+				<HeaderWithSearch />
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">This is Epic News</h1>
+						<h1 className="text-mega mb-8">This is Epic News</h1>
 
 						<div className="flex gap-4">
 							<TeamMemberCard
@@ -79,6 +82,7 @@ export default function App() {
 				<div className="container flex justify-between pb-5">
 					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
 				</div>
+				<FooterLogoCentre />
 			</div>
 		</Document>
 	)
