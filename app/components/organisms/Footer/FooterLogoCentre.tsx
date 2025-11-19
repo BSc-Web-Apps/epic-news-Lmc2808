65effcc9ef2ebfef6a6b526fd0e0738dcc552f61
Lmc2808 <152405599+Lmc2808@users.noreply.github.com>
2025-11-19 12:39:45 +0000

    Added Logo

--- app/components/organisms/Footer/FooterLogoCentre.tsx
+++ app/components/organisms/Footer/FooterLogoCentre.tsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from 'react-router'
-import logo from '#app/assets/svg/icon-placeholder.svg'
+import logo from '#app/assets/svg/Epic News Logo.png'
 import SocialMediaButtons from '#app/components/molecules/SocialMediaButtons'
 import { type FooterProps } from './FooterBasic'
 
--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -1,4 +1,5 @@
 import { useMatches, Link } from 'react-router'
+import logo from '#app/assets/svg/Epic News Logo.png'
 import { SearchBar } from '#app/components/search-bar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
@@ -14,7 +15,9 @@ export default function HeaderWithSearch() {
 		<header className="container py-6">
 			<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
 				<div>
-					<span className="text-foreground text-sm">Epic News</span>
+					<Link to="/" className="flex w-20 items-center justify-center">
+						<img src={logo} />
+					</Link>
 				</div>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
