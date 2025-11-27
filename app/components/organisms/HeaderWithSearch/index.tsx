9c4af272553e8b649c263e30a3ca638a839abdc8
Lmc2808 <152405599+Lmc2808@users.noreply.github.com>
2025-11-27 11:54:33 +0000

    Updated Nav

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -13,8 +13,7 @@ export default function HeaderWithSearch() {
 
 	return (
 		<header className="mt-0 py-0">
-			<nav className="bg-secondary dark:bg-dark-secondary flex flex-wrap items-center justify-between gap-4 p-10 sm:flex-nowrap md:gap-8">
-				<div className="flex items-center gap-10"></div>
+			<nav className="bg-secondary dark:bg-dark-secondary flex flex-wrap items-center justify-between gap-4 p-4 sm:flex-nowrap md:gap-4">
 				<div className="flex items-center gap-4"></div>
 				<Link to="/" className="flex w-20 items-center justify-center">
 					<img src={logo} />
@@ -22,27 +21,31 @@ export default function HeaderWithSearch() {
 
 				<div className="m-5 flex flex-1 gap-8">
 					<div className="m-5 ml-auto hidden max-w-sm flex-1 space-x-4 sm:block">
-						<Link
-							to="/about-us"
-							prefetch="intent"
-							className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
-						>
-							About us
-						</Link>
-						<Link
-							to="/contact-us"
-							prefetch="intent"
-							className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
-						>
-							Contact Us
-						</Link>
-						<Link
-							to="/news"
-							prefetch="intent"
-							className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
-						>
-							News
-						</Link>
+						<div className="container">
+							<div className="border-muted-foreground/75 dark:border-dark-muted-foreground/75 flex flex-col items-center justify-around lg:flex-row lg:pb-8">
+								<Link
+									to="/about-us"
+									prefetch="intent"
+									className="text-secondary-foreground dark:text-dark-secondary-foreground flex flex-col items-start gap-6 py-8 font-bold lg:flex-row"
+								>
+									About us
+								</Link>
+								<Link
+									to="/contact-us"
+									prefetch="intent"
+									className="text-secondary-foreground dark:text-dark-secondary-foreground flex flex-col items-start gap-6 py-8 font-bold lg:flex-row"
+								>
+									Contact Us
+								</Link>
+								<Link
+									to="/news"
+									prefetch="intent"
+									className="text-secondary-foreground dark:text-dark-secondary-foreground flex flex-col items-start gap-6 py-8 font-bold lg:flex-row"
+								>
+									News
+								</Link>
+							</div>
+						</div>
 					</div>
 				</div>
 
