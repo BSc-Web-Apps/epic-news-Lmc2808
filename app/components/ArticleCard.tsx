36c514b37e2dd3c9432e93f08b7770c2ded13eb9
Lmc2808 <152405599+Lmc2808@users.noreply.github.com>
2025-12-11 11:56:14 +0000

    Need to complete other steps

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -1,1 +1,18 @@
-export default function ArticleCard() {}
+type ArticleCard = {
+	title: string
+	category?: string
+}
+
+export default function ArticleCard({ title, category }: ArticleCard) {
+	return (
+		<div className="flex h-48 flex-col justify-between rounded-lg bg-red-900 p-4 transition-transform duration-200 hover:scale-105 hover:bg-red-800">
+			<h3
+				className="mb-2 line-clamp-2 text-lg font-bold text-white"
+				title={title}
+			>
+				{title}
+			</h3>
+			{category && <p className="mt-auto text-sm text-gray-200">{category}</p>}
+		</div>
+	)
+}
